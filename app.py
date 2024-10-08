3f3d8dfb55f6681c52114446d71cecd6ae71fff1
Radi0active17 <134473855+Radi0active17@users.noreply.github.com>
2024-10-08 20:45:09 +0600

    app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+import nltk
+  nltk.download('punkt_tab')
+
+
 from flask import Flask, request, render_template
 from generative_ai import generate_text  # Google Generative AI integration
 from nltk_utils import tokenize, stem, bag_of_words  # NLTK helper functions
